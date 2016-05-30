e60e3731dd43126968b0216aa66d92310c75ce11
wong369 <ewong.369+git@gmail.com>
2016-05-30 15:04:25 -0700

    added function bodies

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m = NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix = function(solve) m <<- solve
+    getmatrix = function() m
+    list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 
 }
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m = x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached matrix")
+        return(m)
+    }
+    data = x$get()
+    m = solve(data, ...)
+    x$setmatrix(m)
+    m
 }
